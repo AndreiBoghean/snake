3993bcc3170c57add52cf68581a6ec4313136063
Andrei Boghean <AndreiBoghean00@gmail.com>
2020-07-19 20:22:35 +0100

    movement + smake length

---
+++ Assets/OnPointGain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Object = UnityEngine.Object;
+
+public class OnPointGain : MonoBehaviour
+{
+    public Rigidbody RB;
+    public Object TailObject;
+    public List<GameObject> TailUnits = new List<GameObject>();
+
+    void Start()
+    {
+        TailUnits.Add((GameObject)Instantiate(TailObject, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0)));
+        var newest = TailUnits.Last();
+        newest.GetComponent<SpringJoint>().connectedBody = RB;
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        { AddTailUnit(); }
+    }
+    public void AddTailUnit()
+    {
+        TailUnits.Add((GameObject)Instantiate(TailObject, TailUnits.Last().transform.position, new Quaternion(0, 0, 0, 0)));
+        TailUnits.Last().GetComponent<SpringJoint>().connectedBody = TailUnits[TailUnits.Count - 2].GetComponent<Rigidbody>();
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody rb;
+    public float Speed = 100f;
+
+
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    private void FixedUpdate()
     {
+        float DeltaSpeed = Speed * Time.deltaTime;
+
+        #region
         
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(new Vector3(0, DeltaSpeed), ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(new Vector3(-DeltaSpeed, 0), ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(new Vector3(0, -DeltaSpeed), ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(new Vector3(DeltaSpeed, 0), ForceMode.VelocityChange);
+        }
+
+        #endregion new movement
+
+        #region old movement
+        /*
+        float x = this.transform.position.x;
+        float y = this.transform.position.y;
+        float z = this.transform.position.z;
+
+        if (Input.GetKey("w"))
+        {
+            this.transform.position = new Vector3(x, y + DeltaSpeed, z);
+        }
+        if (Input.GetKey("a"))
+        {
+            this.transform.position = new Vector3(x - DeltaSpeed, y, z);
+        }
+        if (Input.GetKey("s"))
+        {
+            this.transform.position = new Vector3(x, y - DeltaSpeed, z);
+        }
+        if (Input.GetKey("d"))
+        {
+            this.transform.position = new Vector3(x + DeltaSpeed, y, z);
+        }
+        */
+        #endregion
     }
 }
