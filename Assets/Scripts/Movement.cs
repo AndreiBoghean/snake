e2132ed59922af6c2b24fb4da1cbcef281bb024d
Andrei Boghean <AndreiBoghean00@gmail.com>
2020-07-20 15:55:02 +0100

    done some more stuff, cant remember what
    hi

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,62 +5,93 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public enum MovementMethod
+    { speed, teleport, Gravity }
+    public MovementMethod SelectedMovement;
+
     public Rigidbody rb;
+
     public float Speed = 100f;
-
-
-    void Start()
-    {
-
-    }
+    public float Teleport = 50f;
+    public float Gravity = 9.81f;
     private void FixedUpdate()
     {
-        float DeltaSpeed = Speed * Time.deltaTime;
 
-        #region
-        
-        if (Input.GetKey("w"))
+        switch (SelectedMovement)
         {
-            rb.AddForce(new Vector3(0, DeltaSpeed), ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(new Vector3(-DeltaSpeed, 0), ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(new Vector3(0, -DeltaSpeed), ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(new Vector3(DeltaSpeed, 0), ForceMode.VelocityChange);
-        }
+            case MovementMethod.Gravity:
+                #region gravity
 
-        #endregion new movement
+                if (Input.GetKey("w"))
+                {
+                    Physics.gravity = new Vector3(0, Gravity, 0);
+                }
+                else if (Input.GetKey("a"))
+                {
+                    Physics.gravity = new Vector3(-Gravity, 0, 0);
+                }
+                else if (Input.GetKey("s"))
+                {
+                    Physics.gravity = new Vector3(0, -Gravity, 0);
+                }
+                else if (Input.GetKey("d"))
+                {
+                    Physics.gravity = new Vector3(Gravity, 0, 0);
+                }
+                else
+                { Physics.gravity = Vector3.zero; }
 
-        #region old movement
-        /*
-        float x = this.transform.position.x;
-        float y = this.transform.position.y;
-        float z = this.transform.position.z;
+                System.Console.WriteLine(Physics.gravity);
+                #endregion
+                break;
 
-        if (Input.GetKey("w"))
-        {
-            this.transform.position = new Vector3(x, y + DeltaSpeed, z);
+            case MovementMethod.speed:
+                #region speed
+                float NewSpeed = Speed * Time.deltaTime;
+                if (Input.GetKey("w"))
+                {
+                    rb.AddForce(new Vector3(0, NewSpeed), ForceMode.VelocityChange);
+                }
+                if (Input.GetKey("a"))
+                {
+                    rb.AddForce(new Vector3(-NewSpeed, 0), ForceMode.VelocityChange);
+                }
+                if (Input.GetKey("s"))
+                {
+                    rb.AddForce(new Vector3(0, -NewSpeed), ForceMode.VelocityChange);
+                }
+                if (Input.GetKey("d"))
+                {
+                    Physics.gravity = new Vector3();
+                    rb.AddForce(new Vector3(NewSpeed, 0), ForceMode.VelocityChange);
+                }
+                #endregion
+                break;
+
+            case MovementMethod.teleport:
+                #region teleport
+                float x = this.transform.position.x;
+                float y = this.transform.position.y;
+                float z = this.transform.position.z;
+                float NewDistance = Teleport * Time.deltaTime;
+                if (Input.GetKey("w"))
+                {
+                    this.transform.position = new Vector3(x, y + NewDistance, z);
+                }
+                if (Input.GetKey("a"))
+                {
+                    this.transform.position = new Vector3(x - NewDistance, y, z);
+                }
+                if (Input.GetKey("s"))
+                {
+                    this.transform.position = new Vector3(x, y - NewDistance, z);
+                }
+                if (Input.GetKey("d"))
+                {
+                    this.transform.position = new Vector3(x + NewDistance, y, z);
+                }
+                #endregion
+                break;
         }
-        if (Input.GetKey("a"))
-        {
-            this.transform.position = new Vector3(x - DeltaSpeed, y, z);
-        }
-        if (Input.GetKey("s"))
-        {
-            this.transform.position = new Vector3(x, y - DeltaSpeed, z);
-        }
-        if (Input.GetKey("d"))
-        {
-            this.transform.position = new Vector3(x + DeltaSpeed, y, z);
-        }
-        */
-        #endregion
     }
 }
---
+++ Assets/Scripts/OnPointGain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Object = UnityEngine.Object;
+
+public class OnPointGain : MonoBehaviour
+{
+    public Rigidbody RB;
+    public Object TailObject;
+    public List<GameObject> TailUnits = new List<GameObject>();
+    public float TimeChange;
+
+    void Start()
+    {
+        TailUnits.Add((GameObject)Instantiate(TailObject, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0)));
+        var newest = TailUnits.Last();
+        newest.GetComponent<SpringJoint>().connectedBody = RB;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Tail")
+        {
+            TailUnits.Add(collision.collider.gameObject);
+            TailUnits.Last().GetComponent<SpringJoint>().connectedBody = TailUnits[TailUnits.Count - 2].GetComponent<Rigidbody>();
+            collision.collider.gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+        Unity.Mathematics.Random rnd = new Unity.Mathematics.Random();
+
+        Instantiate(TailObject, new Vector3( rnd.NextInt(-8, 8), rnd.NextInt(-5, 5), 0), new Quaternion(0, 0, 0, 0));
+    }
+}
